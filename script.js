4585ded722d9bb19f37ac22730dedb570c3fe2ea
aaron <afarrell002@gmail.com>
2022-05-01 20:44:51 -0600

    changed handleInput to be asyncronus for css animation, created newTile function upon finishing previous movements, added end of game logic if all tiles are full, added Promsie and promise array to slideTiles fucntion to assist with css transition, and created canMoveDirection functions and canMove function to check if tiles are able to move/merge based on keyDown event

--- script.js
+++ script.js
@@ -12,19 +12,35 @@ const setupInput = () => {
   window.addEventListener("keydown", handleInput, { once: true });
 };
 
-const handleInput = (e) => {
+const handleInput = async (e) => {
   switch (e.key) {
     case "ArrowUp":
-      moveUp();
+      if (!canMoveUp()) {
+        setupInput();
+        return;
+      }
+      await moveUp();
       break;
     case "ArrowDown":
-      moveDown();
+      if (!canMoveDown()) {
+        setupInput();
+        return;
+      }
+      await moveDown();
       break;
     case "ArrowLeft":
-      moveLeft();
+      if (!canMoveLeft()) {
+        setupInput();
+        return;
+      }
+      await moveLeft();
       break;
     case "ArrowRight":
-      moveRight();
+      if (!canMoveRight()) {
+        setupInput();
+        return;
+      }
+      await moveRight();
       break;
     default:
       setupInput();
@@ -32,6 +48,16 @@ const handleInput = (e) => {
   }
 
   grid.cells.forEach((cell) => cell.mergeTiles());
+
+  const newTile = new Tile(gameBoard);
+  grid.randomEmptyCell().tile = newTile;
+
+  if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+    newTile.waitForTransition(true).then(() => {
+      alert("You lose!");
+    });
+    return;
+  }
 
   setupInput();
 };
@@ -53,24 +79,54 @@ const moveRight = () => {
 };
 
 const slideTiles = (cells) => {
-  cells.forEach((group) => {
-    for (let i = 1; i < group.length; i++) {
-      const cell = group[i];
-      if (cell.tile === null) continue;
-      let lastValidCell;
-      for (let j = i - 1; j >= 0; j--) {
-        const moveToCell = group[j];
-        if (!moveToCell.canAccept(cell.tile)) break;
-        lastValidCell = moveToCell;
-      }
-      if (lastValidCell !== null) {
-        if (lastValidCell.tile !== null) {
-          lastValidCell.mergeTile = cell.tile;
-        } else {
-          lastValidCell.tile = cell.tile;
+  return Promise.all(
+    cells.flatMap((group) => {
+      const promises = [];
+      for (let i = 1; i < group.length; i++) {
+        const cell = group[i];
+        if (cell.tile === null) continue;
+        let lastValidCell;
+        for (let j = i - 1; j >= 0; j--) {
+          const moveToCell = group[j];
+          if (!moveToCell.canAccept(cell.tile)) break;
+          lastValidCell = moveToCell;
         }
-        cell.tile = null;
+        if (lastValidCell !== null) {
+          promises.push(cell.tile.waitForTransition());
+          if (lastValidCell.tile !== null) {
+            lastValidCell.mergeTile = cell.tile;
+          } else {
+            lastValidCell.tile = cell.tile;
+          }
+          cell.tile = null;
+        }
       }
-    }
+      return promises;
+    })
+  );
+};
+
+const canMoveUp = () => {
+  return canMove(grid.cellsByColumn);
+};
+
+const canMoveDown = () => {
+  return canMove(grid.cellsByColumn.map((column) => [...column].reverse()));
+};
+const canMoveLeft = () => {
+  return canMove(grid.cellsByRow);
+};
+const canMoveRight = () => {
+  return canMove(grid.cellsByRow.map((row) => [...row].reverse()));
+};
+
+const canMove = (cells) => {
+  return cells.some((group) => {
+    return group.some((cell, index) => {
+      if (index === 0) return false;
+      if (cell.tile === null) return false;
+      const moveToCell = group[index - 1];
+      return moveToCell.canAccept(cell.tile);
+    });
   });
 };
