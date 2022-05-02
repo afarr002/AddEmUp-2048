d3a0e272da1c888dbe02d47258df2d21661fb525
aaron <afarrell002@gmail.com>
2022-05-01 19:15:45 -0600

    created setupInput function to listen for keydown events on the window, created handleInput function with switch statement to handle the switch statement which controls what happens when the keydown events occur

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 import Grid from "./Grid.js";
+import Tile from "./Tile.js";
 
 const gameBoard = document.getElementById("game-board");
 
 const grid = new Grid(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
+setupInput();
+
+const setupInput = () => {
+  window.addEventListener("keydown", handleInput, { once: true });
+};
+
+const handleInput = (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      moveUp();
+      break;
+    case "ArrowDown":
+      moveDown();
+      break;
+    case "ArrowLeft":
+      moveLeft();
+      break;
+    case "ArrowRight":
+      moveRight();
+      break;
+    default:
+      setupInput();
+      return;
+  }
+  setupInput();
+};
+
+const moveUp = () => {
+  slideTiles(grid.cellsByColumn);
+};
