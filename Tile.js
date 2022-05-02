f5e6b4fe919f0e205a49d540b82ff18a3d3e1bc7
aaron <afarrell002@gmail.com>
2022-05-01 19:05:10 -0600

    created setter for tile value

--- Tile.js
+++ Tile.js
@@ -2,12 +2,28 @@ export default class Tile {
   #tileElement;
   #x;
   #y;
+  #value;
 
   constructor(tileConatiner, value = Math.random() > 0.5 ? 2 : 4) {
     this.#tileElement = document.createElement("div");
     this.#tileElement.classList.add("tile");
     tileConatiner.append(this.#tileElement);
     this.value = value;
+  }
+
+  set value(v) {
+    this.#value = v;
+    this.#tileElement.textContent = v;
+    const power = Math.log2(v);
+    const backgroundLightness = 100 - power * 9;
+    this.#tileElement.style.setProperty(
+      "--background-lightness",
+      `${backgroundLightness}%`
+    );
+    this.#tileElement.style.setProperty(
+      "--text-lightness",
+      `${textLightness <= 50 ? 90 : 10}%`
+    );
   }
 
   set x(value) {
